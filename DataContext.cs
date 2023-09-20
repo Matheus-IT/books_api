f10ee0aded7b655fc342c89bab5cbf9bde185df1
Matheus-IT <matheuscosta.tech@gmail.com>
2023-09-20 12:21:24 -0300

    Finally connected to sql server

--- DataContext.cs
+++ DataContext.cs
@@ -1,4 +1,6 @@
 ﻿global using Microsoft.EntityFrameworkCore;
+using BooksApi.Migrations;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace BooksApi
 {
@@ -12,7 +14,7 @@ namespace BooksApi
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=minimalbookdb;Trusted_Connection=true;TrustServerCertificate=true;");
+            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = minimalbookdb; Integrated Security = True; Persist Security Info = False; Pooling = False; Multiple Active Result Sets = False; Encrypt = False; Trust Server Certificate = False; Command Timeout = 0");
         }
 
         public DbSet<Book> Books => Set<Book>();
